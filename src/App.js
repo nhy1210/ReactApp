19be22959b32ce900a0dac13ed79a054f296acb0
Nhy Pham <nhy1210@gmail.com>
2020-02-05 23:04:40 +1100

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import logo from './SSGSLogo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="SSGS" />
+          <p>
+            SSGS IT EDUCON Services Pvt. Ltd.
+          </p>
+          <a
+            className="App-link"
+            href="http://www.ssgsonline.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Go to Website
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
