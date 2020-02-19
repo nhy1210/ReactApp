9dbed2c5bc2db58eeea979caf80b5b02cab94c17
Nhy Pham <nhy1210@gmail.com>
2020-02-19 17:04:49 +1100

    commit tf

--- mongodb.tf
+++ mongodb.tf
@@ -1,7 +1,3 @@
-#variable "mongodbatlas_public_key" {}
-#variable "mongodbatlas_private_key" {}
-#variable "pagerduty_service_key" {}
-
 # Configure the MongoDB Atlas Provider
 provider "mongodbatlas" {
   public_key = "zmsyndzb"
@@ -23,4 +19,3 @@ resource "mongodbatlas_cluster" "GirlGeek" {
   provider_instance_size_name = "M30"
   provider_region_name        = "US_EAST_2"
 }
-#Making connection
